6afea5dbbd88f4d97c7f10f3b8cdce7fe7ff8557
Jessel Diaz <jesseljosuediazperez@hotmail.com>
2022-09-05 11:46:46 -0600

    new article has been added in experience section

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const menu = document.querySelector(".menu")
-const burgerButton = document.getElementById("burger-menu")
-const phone = window.matchMedia("screen and (max-width: 767px)")
-const projectsLink = document.getElementById("link-projects")
-const experienceLink = document.getElementById("link-experience")
-const contactLink = document.getElementById("link-contact")
+const angleDown = document.querySelector(".fa-angle-down");
+const experienceDetails = document.querySelector(".experience-details");
+const burgerButton = document.getElementById("burger-menu");
+const phone = window.matchMedia("screen and (max-width: 767px)");
+const projectsLink = document.getElementById("link-projects");
+const experienceLink = document.getElementById("link-experience");
+const contactLink = document.getElementById("link-contact");
+
+angleDown.addEventListener("click", hideExperienceDetails);
 
 if (phone.matches) {
   burgerButton.addEventListener("click", hideShow)
@@ -30,4 +34,8 @@ function validation() {
 
 function hideShow() {
   menu.classList.toggle("is-active")
+}
+
+function hideExperienceDetails() {
+  experienceDetails.classList.toggle('show-experience-details')
 }
