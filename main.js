da783ac4eee59aa536a1850eda844ebf84bed9b4
Jessel Diaz <jesseljosuediazperez@hotmail.com>
2022-11-21 16:08:48 -0600

    fixed visual details

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const menu = document.querySelector(".menu")
+const experienceTitle = document.querySelector('.experience-title')
 const angleDown = document.querySelector(".fa-angle-down");
-const experienceDetails = document.querySelector(".experience-details");
 const burgerButton = document.getElementById("burger-menu");
 const phone = window.matchMedia("screen and (max-width: 767px)");
 const projectsLink = document.getElementById("link-projects");
 const experienceLink = document.getElementById("link-experience");
 const contactLink = document.getElementById("link-contact");
 
-angleDown.addEventListener("click", hideExperienceDetails);
+experienceTitle.addEventListener("click", (e) => {
+  const experienceDetails = e.target.nextElementSibling;
+  experienceDetails.classList.toggle('show-experience-details')
+});
 
 if (phone.matches) {
   burgerButton.addEventListener("click", hideShow)
@@ -36,6 +39,7 @@ function hideShow() {
   menu.classList.toggle("is-active")
 }
 
-function hideExperienceDetails() {
-  experienceDetails.classList.toggle('show-experience-details')
+function hideExperienceDetails(e) {
+  const experienceDetails = e.target.nextElementSibling;
+  experienceDetails.toggle('show-experience-details')
 }
